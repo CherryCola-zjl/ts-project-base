44244dbbfee415102eebf5fb4d86e82545cbf88a
z <502242791@qq.com>
2024-01-09 18:49:03 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+    },
+    "extends": [
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/recommended',
+        "plugin:react/recommended",
+        'plugin:prettier/recommended'
+    ],
+    "parser": '@typescript-eslint/parser',
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        // ecmaVersion用来指定 ECMAScript 版本
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        '@typescript-eslint', 'import',
+        "react"
+    ],
+    "rules": {
+        'import/no-unresolved': 'off',
+        'import/extensions': 'off',
+    }
+}
---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,29 @@
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from 'vuepress'
+const packageJson = require("../../package.json")
+
+export default defineUserConfig({
+  // 站点配置
+  title: packageJson.name,
+  description: packageJson.description,
+
+  base:'/typescript-sdk-starter/',
+  
+  // 主题和它的配置
+  theme: defaultTheme({
+    navbar:[
+      {
+        text: "首页",
+        link: "/",
+      },
+      {
+        text: "指南",
+        link: "/guide/",
+      },
+      {
+        text: "API",
+        link: "/api/",
+      },
+    ],
+  })
+})
